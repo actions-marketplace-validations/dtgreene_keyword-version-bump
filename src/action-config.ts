f36ec927d8cb88342b88a9d2518dc9046b376022
Dylan Greene <dylan.greene@shipt.com>
2022-04-25 11:48:45 -0500

    Implement search-target

--- src/action-config.ts
+++ src/action-config.ts
@@ -5,47 +5,30 @@ import { exitFailure, loadJson } from './utils';
 type BumpType = {
   type: string;
   keywords: string[];
-  labels: string[];
-};
-
-type GithubAuthor = {
-  name: string;
-  email: string;
 };
 
 export class ActionConfig {
   bumpTypes: BumpType[] = [];
   defaultBumpType: string = '';
   commitMessage: string = '';
-  author: GithubAuthor = {
-    name: '',
-    email: '',
-  };
   constructor() {
     // first, the action configuration from the yaml will be applied
     this.bumpTypes = [
       {
         type: 'major',
         keywords: core.getInput('keywords-major').split(','),
-        labels: [],
       },
       {
         type: 'minor',
         keywords: core.getInput('keywords-minor').split(','),
-        labels: [],
       },
       {
         type: 'patch',
         keywords: core.getInput('keywords-patch').split(','),
-        labels: [],
       },
     ];
     this.defaultBumpType = core.getInput('default-bump-type');
     this.commitMessage = core.getInput('commit-message');
-    this.author = {
-      name: core.getInput('author-name'),
-      email: core.getInput('author-email'),
-    };
     const configPath = core.getInput('configuration');
     if (configPath) {
       const configJson = loadJson(configPath);
@@ -70,23 +53,15 @@ export class ActionConfig {
       if (configJson.commit_message) {
         this.commitMessage = configJson.commit_message;
       }
-      // author
-      if (configJson.author) {
-        this.author.name = configJson.author.name;
-        this.author.email = configJson.author.email;
-      }
     }
 
     // clean up bump types and filter any with no keywords
     this.bumpTypes = this.bumpTypes
-      .map(({ type, keywords, labels }) => ({
+      .map(({ type, keywords }) => ({
         type,
         keywords: this.cleanWords(keywords),
-        labels: this.cleanWords(labels),
       }))
-      .filter(
-        ({ keywords, labels }) => keywords.length > 0 || labels.length > 0
-      );
+      .filter(({ keywords }) => keywords.length > 0);
 
     // verify the config at least has a commit message and either bump types or a default bump type
     try {
--- src/main.ts
+++ src/main.ts
@@ -7,48 +7,22 @@ import {
   execute,
   exitFailure,
   exitSuccess,
-  loadJson,
   getEnv,
 } from './utils';
 import { ActionConfig } from './action-config';
 import { PackageReader } from './package-reader';
 
-type WebhookPayload = {
-  pull_request: {
-    title: string;
-    labels: string[];
-  };
-  head_commit: {
-    message: string;
-  };
-};
-
 export async function run() {
   try {
     // parse our action configuration
     const config = new ActionConfig();
-    // configure git author name
-    if (config.author.name) {
-      execute(`git config --local user.name '${config.author.name}'`);
-    }
-    // configure git author email
-    if (config.author.email) {
-      execute(`git config --local user.email '${config.author.email}'`);
-    }
-    // get the workflow event payload
-    const eventPath = getEnv('GITHUB_EVENT_PATH');
-    const eventJson = loadJson(eventPath) as WebhookPayload;
-    // verify there is an associated pull request
-    assert.ok(
-      eventJson.pull_request,
-      'This event has no associated pull request'
-    );
+    const searchTarget = core.getInput('search-target', { required: true });
     // load the package.json
     const packagePath = path.join(getEnv('GITHUB_WORKSPACE'), 'package.json');
     const packageReader = new PackageReader(packagePath);
     const currentVersion = packageReader.version;
     // get the bumped version
-    const bumpType = getBumpType(config, eventJson);
+    const bumpType = getBumpType(config, searchTarget);
     const bumpedVersion = bumpVersion(packageReader.version, bumpType);
     // update and save the package.json
     packageReader.version = bumpedVersion;
@@ -73,30 +47,17 @@ function getCommitMessage(message: string, version: string) {
   return message.replace(/{version}/g, version);
 }
 
-function getBumpType(config: ActionConfig, payload: WebhookPayload) {
+function getBumpType(config: ActionConfig, target: string) {
   let matchResult = '';
 
   for (let i = 0; i < config.bumpTypes.length; i++) {
-    const { type, keywords, labels } = config.bumpTypes[i];
+    const { type, keywords } = config.bumpTypes[i];
 
     // check the keywords
-    const matchedKeyword = keywords.find((word) =>
-      payload.pull_request.title.includes(word)
-    );
+    const matchedKeyword = keywords.find((word) => target.includes(word));
     if (matchedKeyword) {
       logger.success(
         `Found keyword match: ${matchedKeyword}; for bump type: ${type}`
-      );
-      matchResult = type;
-      break;
-    }
-    // check the labels
-    const matchedLabel = labels.find((word) =>
-      payload.pull_request.labels.includes(word)
-    );
-    if (matchedLabel) {
-      logger.success(
-        `Found label match: ${matchedLabel}; for bump type: ${type}`
       );
       matchResult = type;
       break;
